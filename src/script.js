de440067b8b1844a6658526a7f694bb50dab34d2
Ouzair Ansari <zuber.ansari.997@gmail.com>
2023-07-23 03:27:10 +0530

    initial commit

---
+++ src/script.js
@@ -0,0 +1,19 @@
+window.sections = [...document.querySelectorAll(".section")];
+window.lastScrollTop = window.pageYOffset;
+
+document.body.style.background = window.sections[0].getAttribute("data-bg");
+
+window.addEventListener("scroll", onScroll);
+
+function onScroll() {
+  const scrollTop = window.pageYOffset;
+
+  const section = window.sections
+    .map((section) => {
+      const el = section;
+      const rect = el.getBoundingClientRect();
+      return { el, rect };
+    })
+    .find((section) => section.rect.bottom >= window.innerHeight * 0.5);
+  document.body.style.background = section.el.getAttribute("data-bg");
+}
